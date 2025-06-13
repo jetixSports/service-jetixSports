d11c0ba8ef24d07123a5994d07dd037e6ad5f935
Justin Vegas <justinvegas14@gmail.com>
2025-06-12 20:00:43 -0400

    [ADD]AUTH GUARD agregando guard de session

--- src/guards/auth/auth.guard.ts
+++ src/guards/auth/auth.guard.ts
@@ -5,23 +5,42 @@ import {
   ForbiddenException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+  constructor(private reflector: Reflector,
+    private readonly jwtService: JwtService
+  ) { }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const mode = this.reflector.get<string>('mode', context.getHandler());
-    const body = request.body;
+    const token = request.headers['token-session'];
     if (mode == 'NoAuth') {
       return true;
     }
-    if (!body.token) {
+    if (!token) {
       throw new ForbiddenException({
-        message: 'Token was not sent',
+        message: 'No se envio el Token',
         codeStatus: 400,
       });
     }
-
-    return true;
+    try {
+      const verifyToken = await this.jwtService.verify(token);
+      if (!verifyToken) throw new ForbiddenException({
+        message: 'Token expirado'
+      });;
+      const tokenData = await this.jwtService.decode(token);
+      if(!tokenData?.email || !tokenData._id)
+        throw new ForbiddenException({
+        message: 'Token inválido'
+      });
+      request.userData=tokenData
+      return true;
+    } catch (error) {
+      throw new ForbiddenException({
+        message: 'Token inválido o expirado',
+        codeStatus: 401,
+      });
+    }
   }
 }
